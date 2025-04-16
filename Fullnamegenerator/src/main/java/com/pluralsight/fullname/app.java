519c6958ede1094c7972d75e0e6662e86ae16d4e
FIRST_NAME LAST_NAME <my_name@example.com>
2025-04-16 15:31:21 -0400

    complete full name generator 1

---
+++ Fullnamegenerator/src/main/java/com/pluralsight/fullname/app.java
@@ -0,0 +1,38 @@
+package com.pluralsight.fullname;
+
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first name");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter your middle name");
+        String middleName = scanner.nextLine();
+
+        System.out.println("Enter your last name");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter your suffix (press enter to skip): ");
+        String suffix = scanner.nextLine();
+
+        System.out.println("Your full name is: " + firstName + " " + middleName + " " + lastName + " " + suffix);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
